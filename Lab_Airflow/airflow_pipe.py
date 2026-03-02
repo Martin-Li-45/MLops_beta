a26639bc9337aa464e5756e5fa5a40ce5aa3b64d
Svyatoslav Meshkov <svyatoslav.meshkov12@gmail.com>
2026-03-02 16:31:10 +0500

    fix1

--- Lab_Airflow/airflow_pipe.py
+++ Lab_Airflow/airflow_pipe.py
@@ -136,8 +136,8 @@ def clear_data():
 dag_cars = DAG(
     dag_id="phone_price_prediction",
     start_date=datetime(2025, 2, 3),
-    concurrency=4,
-    schedule_interval=timedelta(minutes=30),
+    max_active_tasks=4,
+    schedule=timedelta(minutes=30),
     max_active_runs=1,
     catchup=False,
 )
